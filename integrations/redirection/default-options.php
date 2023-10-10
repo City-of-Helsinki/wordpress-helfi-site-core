a6219aebda220250c51744e42ba6c2950f0d62f8
Antti Koskinen <koskinenaa@users.noreply.github.com>
2023-10-10 16:07:57 +0300

    Update default-options.php

--- integrations/redirection/default-options.php
+++ integrations/redirection/default-options.php
@@ -9,7 +9,66 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
   * Init
   */
-add_action( 'helsinki_site_core_loaded', __NAMESPACE__ . '\\init' );
+add_action( 'helsinki_site_core_loaded', __NAMESPACE__ . '\\init', 11 );
 function init(): void {
+	if ( ! is_redirection_options_available() ) {
+		return;
+	}
 
+	add_filter( 'redirection_log_data', __NAMESPACE__ . '\\filter_redirection_log_data', 9999 );
+	add_filter( 'redirection_404_data', __NAMESPACE__ . '\\filter_redirection_log_data', 9999 );
+
+	add_filter( 'red_default_options', __NAMESPACE__ . '\\filter_redirection_options', 9999 );
+	add_filter( 'redirection_save_options', __NAMESPACE__ . '\\filter_redirection_options', 9999 );
+	add_filter( redirection_option_filter(), __NAMESPACE__ . '\\filter_redirection_get_options', 9999 );
+}
+
+/**
+  * Conditionals
+  */
+function is_redirection_options_available(): bool {
+	return defined( 'REDIRECTION_OPTION' ) && REDIRECTION_OPTION;
+}
+
+/**
+  * Configuration
+  */
+function options_overwrite(): array {
+	return [
+		'ip_logging' => 0,
+		'log_header' => false,
+	];
+}
+
+function log_data_overwrite(): array {
+	return [
+		'ip' => '',
+		'request_data' => '',
+	];
+}
+
+function redirection_option_filter(): string {
+	return 'option_' . REDIRECTION_OPTION;
+}
+
+/**
+  * Options
+  */
+function filter_redirection_options( array $options ): array {
+	return array_merge( $options, options_overwrite() );
+}
+
+function filter_redirection_get_options( $options ): array {
+	if ( ! is_array( $options ) ) {
+		return $options;
+	}
+
+	return array_merge( $options, options_overwrite() );
+}
+
+/**
+  * Log entries
+  */
+function filter_redirection_log_data( array $data ): array {
+	return array_merge( $data, log_data_overwrite() );
 }
