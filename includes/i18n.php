60b8d19fdefc8e6a108f258fc1d22be790f28894
Antti Koskinen <koskinenaa@users.noreply.github.com>
2023-10-10 11:47:38 +0300

    Update i18n.php

--- includes/i18n.php
+++ includes/i18n.php
@@ -7,9 +7,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 function textdomain() {
-	load_plugin_textdomain(
-		'helsinki-site-core',
-		false,
-		apply_filters( 'helsinki_site_core_plugin_dirname', '' ) . '/languages'
-	);
+	if ( apply_filters( 'helsinki_site_core_is_must_use_plugin', false ) ) {
+		load_muplugin_textdomain(
+			'helsinki-site-core',
+			apply_filters( 'helsinki_site_core_plugin_dirname', '' ) . '/languages'
+		);
+	} else {
+		load_plugin_textdomain(
+			'helsinki-site-core',
+			false,
+			apply_filters( 'helsinki_site_core_plugin_dirname', '' ) . '/languages'
+		);
+	}
 }
