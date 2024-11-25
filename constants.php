c5851da0d82bd88eb0c71f93b6a5b472bc2e56cb
Antti Koskinen <koskinenaa@users.noreply.github.com>
2024-11-25 13:51:45 +0200

    Update constants.php

--- constants.php
+++ constants.php
@@ -14,7 +14,7 @@ function define_constants(string $file) : void {
 		require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 	}
 
-	$pluginData = get_plugin_data( $file );
+	$pluginData = get_plugin_data( $file, false, false );
 	$dirname = dirname( $file );
 	$basename = basename( $file );
 	$dirbasename = basename( $dirname );
