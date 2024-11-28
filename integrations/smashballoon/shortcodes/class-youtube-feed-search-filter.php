b55ca9089fdcc4bfbcb50c87c64438957b92fecc
Antti Koskinen <koskinenaa@users.noreply.github.com>
2024-11-28 18:43:59 +0200

    Create class-youtube-feed-search-filter.php

---
+++ integrations/smashballoon/shortcodes/class-youtube-feed-search-filter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Helsinki\WordPress\Site\Core\Integrations\SmashBalloon\Shortcodes;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class YouTube_Feed_Search_Filter extends Abstract_Shortcode_Bypasser
+{
+	protected function social_feed_wrap_type(): string
+	{
+		return 'youtube';
+	}
+
+	protected function screen_reader_content( array $attributes ): string
+	{
+		return '';
+	}
+}
