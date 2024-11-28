1bf578e0a7bde58d7feae037c63ad5b71ab434d8
Antti Koskinen <koskinenaa@users.noreply.github.com>
2024-11-28 18:43:27 +0200

    Create class-instagram-feed-adapter.php

---
+++ integrations/smashballoon/adapters/class-instagram-feed-adapter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Helsinki\WordPress\Site\Core\Integrations\SmashBalloon\Adapters;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class Instagram_Feed_Adapter implements Social_Feed_Adapter_Interface
+{
+	public function __construct()
+	{
+
+	}
+
+	public function render_source(): string
+	{
+		return '';
+	}
+}
