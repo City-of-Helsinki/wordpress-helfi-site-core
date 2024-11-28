7090fd55c1a58385aba4af6c5976a51d8fb6d8ab
Antti Koskinen <koskinenaa@users.noreply.github.com>
2024-11-28 19:50:00 +0200

    Update class-instagram-feed-adapter.php

--- integrations/smashballoon/adapters/class-instagram-feed-adapter.php
+++ integrations/smashballoon/adapters/class-instagram-feed-adapter.php
@@ -8,13 +8,38 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Instagram_Feed_Adapter implements Social_Feed_Adapter_Interface
 {
-	public function __construct()
-	{
+	protected string $name;
 
+	public function __construct( string $name )
+	{
+		if ( ! $name ) {
+			throw new \InvalidArgumentException(
+				__( 'Instagram username required.', 'helsinki-site-core' )
+			);
+		}
+
+		$this->name = trim( $name );
 	}
 
 	public function render_source(): string
 	{
-		return '';
+		return sprintf(
+			'<a href="%1$s" rel="noopener">%2$s</a>',
+			esc_url( $this->link_url() ),
+			esc_html( $this->anchor_text() )
+		);
+	}
+
+	protected function link_url(): string
+	{
+		return 'https://www.instagram.com/' . mb_strtolower( $this->name ) . '/';
+	}
+
+	protected function anchor_text(): string
+	{
+		return sprintf(
+			_x( 'Follow %1$s on Instagram', '%1$s: profile name', 'helsinki-site-core' ),
+			$this->name
+		);
 	}
 }
