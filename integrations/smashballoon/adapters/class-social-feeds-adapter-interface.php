1d6477a1bd78d82c1c5b723aaf1e5564de8dc389
Antti Koskinen <koskinenaa@users.noreply.github.com>
2024-12-06 15:43:18 +0200

    Update class-social-feeds-adapter-interface.php

--- integrations/smashballoon/adapters/class-social-feeds-adapter-interface.php
+++ integrations/smashballoon/adapters/class-social-feeds-adapter-interface.php
@@ -8,9 +8,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 interface Social_Feeds_Adapter_Interface
 {
-	public function composite_feed( array $attributes ): ?Social_Feed_Adapter_Interface;
-	public function facebook_feed( array $attributes ): ?Social_Feed_Adapter_Interface;
-	public function instagram_feed( array $attributes ): ?Social_Feed_Adapter_Interface;
-	public function twitter_feed( array $attributes ): ?Social_Feed_Adapter_Interface;
-	public function youtube_feed( array $attributes ): ?Social_Feed_Adapter_Interface;
+	public function composite_feed( array $attributes ): Social_Feed_Adapter_Interface;
+	public function facebook_feed( array $attributes ): Social_Feed_Adapter_Interface;
+	public function instagram_feed( array $attributes ): Social_Feed_Adapter_Interface;
+	public function twitter_feed( array $attributes ): Social_Feed_Adapter_Interface;
+	public function youtube_feed( array $attributes ): Social_Feed_Adapter_Interface;
 }
