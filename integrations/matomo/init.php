96df50ef30576eb4e15715b2355985c850e9b744
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-01-30 16:19:41 +0200

    Create init.php

---
+++ integrations/matomo/init.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Helsinki\WordPress\Site\Core\Integrations\Matomo;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+\add_action( 'helsinki_site_core_loaded', __NAMESPACE__ . '\\init', 11 );
+function init(): void {
+	\add_action( 'admin_init', __NAMESPACE__ . '\\register_matomo_settings' );
+	\add_action( 'admin_menu', __NAMESPACE__ . '\\register_menu_page' );
+
+	\add_action( 'helsinki_site_core_matomo_admin_page', __NAMESPACE__ . '\\menu_page_layout', 1 );
+	\add_action( 'helsinki_site_core_matomo_admin_page_content', __NAMESPACE__ . '\\menu_page_title', 10 );
+	\add_action( 'helsinki_site_core_matomo_admin_page_content', __NAMESPACE__ . '\\settings_form', 10 );
+}
