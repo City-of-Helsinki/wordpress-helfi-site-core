9a9d8a445cd2964d174f3fed4a72e2a2793054e0
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-01-31 13:41:11 +0200

    Create complianz.php

---
+++ integrations/matomo/complianz.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Helsinki\WordPress\Site\Core\Integrations\Matomo;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+\add_filter( 'cmplz_known_script_tags', __NAMESPACE__ . '\\provide_cmplz_matomo_script' );
+function provide_cmplz_matomo_script( array $tags ): array {
+	$config = tracking_code_config( setting_values() );
+
+	$tags[] = array(
+		'name' => 'helsinki_matomo',
+		'category' => 'statistics',
+		'urls' => array( $config->url ),
+	);
+
+	return $tags;
+}
+
+\add_filter( 'cmplz_detected_services', __NAMESPACE__ . '\\provide_cmplz_detected_matomo' );
+function provide_cmplz_detected_matomo( $statistics ): array {
+	if ( ! in_array( 'matomo', $statistics ) ) {
+		$statistics[] = 'matomo';
+	}
+
+	return $statistics;
+}
