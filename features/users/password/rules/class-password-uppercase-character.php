c4a2ac5b2c906dbf8fb2744ce0e04adbbd189f8a
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-05-14 23:46:49 +0300

    Create class-password-uppercase-character.php

---
+++ features/users/password/rules/class-password-uppercase-character.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Helsinki\WordPress\Site\Core\Features\Users\Password\Rules;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class Password_Uppercase_Character implements Password_Rule_Interface
+{
+	public function hint(): string
+	{
+		return __( 'At least one uppercase character, A-Z.', 'helsinki-site-core' );
+	}
+
+	public function validate( string $password ): void
+	{
+		if ( 1 !== preg_match( '/[a-z]/', $password ) ) {
+			throw new \InvalidArgumentException(
+				__( 'The password does not contain an uppercase character.', 'helsinki-site-core' )
+			);
+		}
+	}
+}
