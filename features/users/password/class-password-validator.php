32dd3413e99aaa1c030d9d19219630cc0d7e4624
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-05-14 23:47:09 +0300

    Create class-password-validator.php

---
+++ features/users/password/class-password-validator.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Helsinki\WordPress\Site\Core\Features\Users\Password;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+use Helsinki\WordPress\Site\Core\Features\Users\Password\Rules\Password_Rule_Interface;
+use Exception;
+
+class Password_Validator
+{
+	private array $rules;
+
+	public function __construct(
+		Password_Rule_Interface ...$rules
+	) {
+		$this->rules = $rules;
+	}
+
+	public function hints(): array
+	{
+		return array_map(
+			fn( Password_Rule_Interface $rule ) => $rule->hint(),
+			$this->rules
+		);
+	}
+
+	public function validate( string $password ): void
+	{
+		$rules_to_check = count( $this->rules );
+
+		$invalid_rules = null;
+
+		for ( $i=0; $i < $rules_to_check; $i++ ) {
+			try {
+				$this->rules[$i]->validate( $password );
+			} catch ( Exception $invalid_rule ) {
+				$invalid_rules = new Exception(
+					message: $invalid_rule->getMessage(),
+					code: $invalid_rule->getCode(),
+					previous: $invalid_rules
+				);
+			}
+		}
+
+		if ( $invalid_rules ) {
+			throw $invalid_rules;
+		}
+	}
+}
